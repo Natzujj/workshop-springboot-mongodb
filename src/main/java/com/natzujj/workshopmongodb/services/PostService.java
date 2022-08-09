8aca7c3e2faebd6f1209e3c7303bceec51ffd3e5
Natzujj <gabrielnatsujake@gmail.com>
2022-08-09 12:00:18 -0300

    Post findById

---
+++ src/main/java/com/natzujj/workshopmongodb/services/PostService.java
@@ -0,0 +1,26 @@
+package com.natzujj.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natzujj.workshopmongodb.domain.Post;
+import com.natzujj.workshopmongodb.repository.PostRepository;
+import com.natzujj.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found!"));
+	}
+	
+
+	
+}
